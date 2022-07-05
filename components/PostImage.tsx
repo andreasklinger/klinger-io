ed0855678c68a8624b82fe165f0e4cc28bf81509
Fabian Hiller <hillerfabian11@gmail.com>
2022-07-05 13:25:54 +0200

    Add className prop to PostImage component

--- components/PostImage.tsx
+++ components/PostImage.tsx
@@ -2,13 +2,15 @@ import { FC } from 'react';
 import { Image } from './Image';
 
 interface PostImageProps {
+  className?: string;
   src?: string;
   alt?: string;
 }
 
-export const PostImage: FC<PostImageProps> = ({ src, alt }) =>
+export const PostImage: FC<PostImageProps> = ({ className, src, alt }) =>
   src ? (
     <Image
+      className={className}
       src={`/images/${src}`}
       sizes="(max-width: 768px) 95vw, 728px"
       loading="lazy"
